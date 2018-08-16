077d4626aa499f2dc5a3bbec149c8330c3666ba9
Colten Jackson <jazzyjackson@gmail.com>
2018-08-15 22:01:22 -0400

    initial commit

---
+++ transflect.js
@@ -0,0 +1,9 @@
+const {Transform} = require('stream')
+const url = require('url')
+const querystring = require('querystring')
+
+module.exports = class Transflect extends Transform {
+    constructor(){
+        super()
+    }
+}
